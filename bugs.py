0748362649e5f45711066814579a7148543cbb72
Lindsay Goins <lindsayggoins@gmail.com>
2021-07-02 16:27:38 -0600

    Finished bug web scraper

--- bugs.py
+++ bugs.py
@@ -16,8 +16,34 @@ def get_bugs():
         text = elem.text.strip()
         if text == "Tips & Tricks":
             break
+        elif "Season" in text:
+            continue
+        elif "★" in text:
+            text = text[5:]
+            bugs.append(text)
         else:
             bugs.append(text)
-    return bugs[12:]
+    return bugs[12:332]
 
-print(get_bugs())
+
+def parse_data():
+    bugs = get_bugs()
+    bug_dict = {}
+    for i in range(80):
+        offset = i * 4
+        bug_dict[bugs[offset]] = bugs[1 + offset:4 + offset]
+    return bug_dict
+
+
+def create_file():
+    bugs = parse_data()
+    f = open("data/bugs.txt", "a")
+    for key in bugs:
+        query = "(" + key + ","
+        for item in bugs[key]:
+            query += item + ","
+        query += "0" + ")" + ","
+        f.write(query)
+    f.close()
+
+create_file()
