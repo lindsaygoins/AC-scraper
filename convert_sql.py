067b3096dfd798d3fb3ec8cdc000da4b7e4fc997
Lindsay Goins <lindsayggoins@gmail.com>
2021-07-11 21:16:03 -0600

    Updated data

--- convert_sql.py
+++ convert_sql.py
@@ -4,45 +4,18 @@ def convert_sql(file_name):
     f = open(file_name, 'r')
     line = f.read()
     pieces = line.split(',')
-    rugs = pieces[:545]
-    wall_floor = pieces[545:]
 
-    count_name = 5
-    count_category = 4
-    count_size = 3
-    for i, rug in enumerate(rugs):
-        if count_name == 5:
-            rugs[i] = rug[0] + '"' + rug[1:] + '"'
+    count_name = 3
+    for i, piece in enumerate(pieces):
+        if count_name == 3:
+            pieces[i] = piece[0] + '"' + piece[1:] + '"'
             count_name = 0
-        elif count_category == 5:
-            rugs[i] = '"' + rug + '"'
-            count_category = 0
-        elif count_size == 5:
-            rugs[i] = '"' + rug + '"'
-            count_size = 0
         count_name += 1
-        count_category += 1
-        count_size += 1
-
-    count_name = 4
-    count_category = 3
-    for i, w_f in enumerate(wall_floor):
-        if count_name == 4:
-            wall_floor[i] = w_f[0] + '"' + w_f[1:] + '"'
-            count_name = 0
-        elif count_category == 4:
-            wall_floor[i] = '"' + w_f + '"'
-            count_category = 0
-        count_name += 1
-        count_category += 1
-
-    pieces = rugs + wall_floor
     pieces = ','.join(pieces)
     f.close()
 
-
-    f = open("data/final/items.txt", "a")
+    f = open("data/final/sea_creatures.txt", "a")
     f.write(pieces)
     f.close()
 
-convert_sql("data/lyrics saharah_items.txt")
+convert_sql("data/sea_creatures.txt")
